f4c5926c1e22a1f57fd1812d5beaf77a381f7663
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-09-28 16:21:36 +0330

    add file reader

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // accepting arguments
     let arguments: Vec<String> = env::args().collect();
-    
+
     let query = &arguments[1];
     let filename = &arguments[2];
 
-    println!("searching for \"{}\" in file: \"{}\" ...", query, filename)
+    println!("searching for \"{}\" in file: \"{}\" ...", query, filename);
+
+    let file_contents =
+        fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("____________________");
+    println!("With text: {}", file_contents);
 }
