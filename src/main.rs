43939259efb77d8de989ed840e2dde25441c0c65
Amirhossein Azimyzadeh <amirhosseinazimyzadeh2@gmail.com>
2021-09-29 13:52:30 +0330

    add config struct and move logic to the run() function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // accepting arguments
     let arguments: Vec<String> = env::args().collect();
 
-    let query = &arguments[1];
-    let filename = &arguments[2];
+    let config = Config::new(&arguments).unwrap_or_else(|error| {
+        println!("Problem in parsing arguments: \n\t - {}", error);
+        process::exit(1);
+    });
 
-    println!("searching for \"{}\" in file: \"{}\" ...", query, filename);
+    println!("searching for \"{}\" in file: \"{}\" ...", config.query, config.filename);
+    run(config);
+}
 
-    let file_contents =
-        fs::read_to_string(filename).expect("Something went wrong reading the file");
+fn run(config: Config) {
+    let file_contents = fs::read_to_string(config.filename)
+        .expect("something went wrong in reading file");
+    
+    println!("file content: {}", file_contents);
+}
 
-    println!("____________________");
-    println!("With text: {}", file_contents);
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query: String = args[1].clone();
+        let filename: String = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
